f6803231a199dfaa44e11575f4869e48dc9c5a49
Babolow <vigerthomas76@gmail.com>
2017-02-21 15:54:07 +0100

    add data access

---
+++ src/LuckyProject_API/DataAccess/Base_testContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using LuckyProject_API.Entities;
+
+namespace LuckyProject_API.DataAccess
+{
+    public partial class Base_testContext : DbContext
+    {
+        public virtual DbSet<Utilisateur> Utilisateur { get; set; }
+
+        // Unable to generate entity type for table 'dbo.Table_test'. Please see the warning messages.
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+            optionsBuilder.UseSqlServer(@"Server=servertest1234.database.windows.net;Database=Base_test;user id=user_admin;password=e4g5G2ce");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Utilisateur>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .HasColumnName("ID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Age).HasColumnName("age");
+
+                entity.Property(e => e.Nom)
+                    .HasColumnName("nom")
+                    .HasColumnType("varchar(100)");
+
+                entity.Property(e => e.Prenom)
+                    .HasColumnName("prenom")
+                    .HasColumnType("varchar(100)");
+
+                entity.Property(e => e.Ville)
+                    .HasColumnName("ville")
+                    .HasColumnType("varchar(255)");
+            });
+        }
+    }
+}
---
+++ src/LuckyProject_API/Entities/Utilisateur.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LuckyProject_API.Entities
+{
+    public partial class Utilisateur
+    {
+        public int Id { get; set; }
+        public string Prenom { get; set; }
+        public string Nom { get; set; }
+        public string Ville { get; set; }
+        public int? Age { get; set; }
+    }
+}

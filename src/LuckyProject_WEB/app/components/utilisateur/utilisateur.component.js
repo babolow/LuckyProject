55ac4351cc73c391fc9e2f95d5f430af664e060e
Babolow <vigerthomas76@gmail.com>
2017-02-22 16:45:03 +0100

    add ui grid

--- src/LuckyProject_WEB/app/components/utilisateur/utilisateur.component.js
+++ src/LuckyProject_WEB/app/components/utilisateur/utilisateur.component.js
@@ -1,12 +1,10 @@
 ﻿import './utilisateur.html';
-//import {UtilisateurController } from './utilisateur.controller';
 
 class UtilisateurController {
     constructor($http, UtilisateurService) {
         'ngInject';
 
-      //  this.userList = '[{"id":1,"prenom":"Rébecca","nom":"Armand","ville":"Saint-Didier-des-Bois","age":24},{"id":2,"prenom":"Aimée","nom":"Hebert","ville":"Marigny-le-Châtel","age":36},{"id":3,"prenom":"Marielle","nom":"Ribeiro","ville":"Maillères","age":27},{"id":4,"prenom":"Hilaire","nom":"Savary","ville":"Conie-Molitard","age":58}]';
-        this.userService = UtilisateurService;
+      this.userService = UtilisateurService;
     }
 
     $onInit() {
@@ -15,15 +13,6 @@ class UtilisateurController {
 
     }
 
-    //getListUtilisateur($http) {
-    //    $http({
-    //        method: 'GET',
-    //        url: 'http://localhost:20570/api/utilisateur'
-    //    }).then((response) => {
-    //        this.userList = response.data;
-    //    },
-    //    (error) => {console.log(error); });
-    //}
 };
 
 
